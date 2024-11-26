c7f59f6a70ff8b200c62018f8b5d92cd1d1f72ae
Eugene Rusalev <eugene.rusalev@jetbi.com>
2024-11-26 11:17:48 +0300

    Update the implementation

--- custom_components/meibes-servo-drive/cover.py
+++ custom_components/meibes-servo-drive/cover.py
@@ -1,18 +1,20 @@
 import asyncio
-from homeassistant.helpers import entity_platform
 from homeassistant.components.cover import CoverEntity
-from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.core import HomeAssistant
+from homeassistant.components.cover import CoverEntityFeature
 
 class MeibesServoDrive(CoverEntity):
-    def __init__(self, name, relay_on, relay_off, max_time_to_move=30):
+    def __init__(self, name: str, switch_plus: str, switch_minus: str, position_entity: str = 0, max_time_to_move: int = 30):
         """Initialize the servo drive."""
         self._name = name
-        self._relay_on = relay_on  # Relay activation function
-        self._relay_off = relay_off  # Relay deactivation function
+        self._switch_plus = switch_plus  # Switch activation function
+        self._switch_minus = switch_minus  # Switch deactivation function
         self._position = 0  # Initial position of the servo drive (0 - closed)
         self._target_position = 0  # Target position
         self._is_moving = False  # Movement status
         self._max_time_to_move = max_time_to_move  # Maximum time for movement (in seconds)
+        self._position_entity = position_entity
 
     @property
     def name(self):
@@ -20,13 +22,17 @@ class MeibesServoDrive(CoverEntity):
 
     @property
     def current_cover_position(self):
-        """Returns the current position of the servo drive from 0 to 10."""
+        """Returns the current position of the servo drive from 0 to 100."""
+        """Get current position from input_number state."""
+        state = self.hass.states.get(self._position_entity)
+        if state:
+            self._position = int(state.state)
         return self._position
 
     @property
     def is_open(self):
-        """Checks if the servo drive is open (position 10)."""
-        return self._position == 10
+        """Checks if the servo drive is open (position > 0)."""
+        return self._position > 0
 
     @property
     def is_closed(self):
@@ -38,37 +44,65 @@ class MeibesServoDrive(CoverEntity):
         """Returns the movement status."""
         return self._is_moving
 
-    async def async_open(self):
-        """Open the servo drive (to position 10)."""
+    @property
+    def supported_features(self):
+        return CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE | CoverEntityFeature.SET_POSITION | CoverEntityFeature.STOP
+
+    async def async_open_cover(self):
+        """Open the servo drive (to position 100)."""
         if not self._is_moving:
-            self._target_position = 10
+            self._target_position = 100
             await self._move_servo()
 
-    async def async_close(self):
+    async def async_close_cover(self):
         """Close the servo drive (to position 0)."""
         if not self._is_moving:
             self._target_position = 0
             await self._move_servo()
 
     async def async_set_cover_position(self, position):
-        """Set the position of the servo drive from 0 to 10."""
-        self._target_position = max(0, min(10, position))  # Limit the value between 0 and 10
+        """Set the position of the servo drive from 0 to 100."""
+        self._target_position = max(0, min(100, position))  # Limit the value between 0 and 100
+        await self._move_servo()
+
+    async def async_stop_cover(self):
+        if self._is_moving:
+            if self.hass.states.is_state(self._switch_minus, "on"):
+                await self._toggle_switch_state(self._switch_minus, "off")
+            elif self.hass.states.is_state(self._switch_plus, "on"):
+                await self._toggle_switch_state(self._switch_plus, "off")
+            self._position = self._target_position
+            self.is_moving = False
+        self.schedule_update_ha_state()
+
+    async def async_reset_position(self):
+        """Reset the servo drive position in case of desynchronization."""
+        self._is_moving = True
+        await self._toggle_switch_state(self._switch_plus, "on")
+        # Wait for the movement to complete
+        await asyncio.sleep(self._max_time_to_move)
+        # Turn off swith
+        await self._toggle_switch_state(self._switch_plus, "off")
+        self._is_moving = False
+        self._position = 100
+        self._target_position = 50
         await self._move_servo()
 
     async def _move_servo(self):
         """Move the servo to the target position using time delays."""
         if self._target_position == self._position:
+            self._is_moving = False
             return  # Already in the target position, no need to move
 
         self._is_moving = True
-        movement_time = abs(self._target_position - self._position) * self._max_time_to_move / 10  # Calculate movement time
+        movement_time = abs(self._target_position - self._position) * self._max_time_to_move / 100  # Calculate movement time
 
         if self._target_position > self._position:
             # Move towards opening
-            await self._relay_on()
+            await self._toggle_switch_state(self._switch_plus, "on")
         else:
             # Move towards closing
-            await self._relay_off()
+            await self._toggle_switch_state(self._switch_minus, "on")
 
         # Wait for the movement to complete
         await asyncio.sleep(movement_time)
@@ -77,36 +111,70 @@ class MeibesServoDrive(CoverEntity):
         self._position = self._target_position
         self._is_moving = False
 
-        # Turn off relay once movement is completed
-        await self._relay_off() if self._position == 0 else self._relay_on()
+        # Turn off switch once movement is completed
+        if self.hass.states.is_state(self._switch_minus, "on"):
+            await self._toggle_switch_state(self._switch_minus, "off")
+        else:
+            await self._toggle_switch_state(self._switch_plus, "off")
+        self.schedule_update_ha_state()
+        await self._save_position()
 
-    async def async_reset_position(self):
-        """Reset the servo drive position in case of desynchronization."""
-        # In case of desynchronization, manually set the correct position
-        self._position = self._target_position
-        self._is_moving = False
-        await self._relay_off()  # Stop the relay if it was on
 
-    async def async_setup_services(hass: HomeAssistant):
-        """Set up the services for the servo servo drive."""
-        platform = entity_platform.async_get_current_platform()
+    async def _toggle_switch_state(self, switch_entity_id: str, state: str):
+        """Toggle the state of the switch."""
+        """Turn the switch on or off."""
+        if state == "on":
+            await self.hass.services.async_call("homeassistant", "turn_on", {"entity_id": switch_entity_id})
+        elif state == "off":
+            await self.hass.services.async_call("homeassistant", "turn_off", {"entity_id": switch_entity_id})
 
-        platform.async_register_entity_service(
-            "open_servo_drive",
-            {},
-            "async_open",
+    async def _save_position(self):
+        """Save the current position of the servo to input_number."""
+        await self.hass.services.async_call(
+            "input_number",
+            "set_value",
+            {"entity_id": self._position_entity, "value": self._position},
         )
 
-        platform.async_register_entity_service(
-            "close_servo_drive",
-            {},
-            "async_close",
-        )
 
-        platform.async_register_entity_service(
-            "set_servo_drive_position",
-            {
-                "position": int,
-            },
-            "async_set_cover_position",
-        )
+async def async_setup_platform(
+    hass: HomeAssistant, config: dict, async_add_entities: AddEntitiesCallback, discovery_info=None
+):
+    """Setup the platform for integrating with the servo drive."""
+    switch_plus = config.get("switch_plus")  # Get switch plus from configuration.yaml
+    switch_minus = config.get("switch_minus")  # Get switch minus from configuration.yaml
+    max_time_to_move = config.get("max_time_to_move")  # Get max_time_to_move from configuration.yaml
+    name = config.get("name", "Meibes Servo Drive")  # Default name if not provided
+    position_entity = config.get("position_entity", "input_number.meibes_servo_position")  #Get entity that keeps current position
+
+    # Check that both switches are defined
+    if switch_plus is None or switch_minus is None:
+        raise ValueError("Both 'switch_plus' and 'switch_minus' must be defined in the configuration.")
+
+    # Create the servo drive entity
+    servo_drive = MeibesServoDrive(name, switch_plus, switch_minus, position_entity, max_time_to_move)
+
+    # Add entity to Home Assistant
+    async_add_entities([servo_drive])
+
+    # Registering services (e.g., open_servo_drive, close_servo_drive, set_servo_drive_position)
+    async def open_servo_drive(call):
+        """Open the servo drive."""
+        await servo_drive.async_open_cover()
+
+    async def close_servo_drive(call):
+        """Close the servo drive."""
+        await servo_drive.async_close_cover()
+
+    async def set_servo_drive_position(call):
+        """Set the position of the servo drive."""
+        position = call.data.get("position")
+        await servo_drive.async_set_cover_position(position)
+
+    async def reset_position(call):
+        await servo_drive.async_reset_position()
+
+    hass.services.async_register("meibes_servo", "open_servo_drive", open_servo_drive)
+    hass.services.async_register("meibes_servo", "close_servo_drive", close_servo_drive)
+    hass.services.async_register("meibes_servo", "set_servo_drive_position", set_servo_drive_position)
+    hass.services.async_register("meibes_servo", "reset_servo_drive_position", reset_position)
